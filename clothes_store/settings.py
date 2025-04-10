735d07d5921e632a441fcb52a9a0fc48defddd67
vorvikqq <delodonn@ukr.net>
2025-04-10 13:10:46 +0300

    add products urls to our project

--- clothes_store/settings.py
+++ clothes_store/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#!!! Прочитати про це !!!!
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 # Default primary key field type
---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import popular_list, product_detail
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', popular_list, name='popular_list'),
+    path('<slug:slug>/', product_detail, name='product_detail'),
+]
