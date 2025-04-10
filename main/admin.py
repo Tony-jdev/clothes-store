9cabb9bfb5915f3cde70891c16a2fa332c991aa1
vorvikqq <delodonn@ukr.net>
2025-04-10 12:27:13 +0300

    add Product and Categories models to admin panel

---
+++ main/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Category, Product
+
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'name', 'is_visible', 'sort')
+    list_display_links = ('name', 'id')
+    list_editable = ('is_visible',  'sort')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'name', 'price', 'discount', 'is_available', 'sort', 'created_at', 'updated_at')
+    list_display_links = ('name', 'id')
+    list_editable = ('is_available', 'sort', 'price', 'discount')
+
+    list_filter = ['is_available','created_at', 'updated_at']
