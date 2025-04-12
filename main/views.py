b7ec59b925b57c737de21eae085ec14b0d997f4c
xanaxhol1c <loram.hoptan@gmail.com>
2025-04-12 16:29:35 +0300

    added pagination and changed context list for porducts-list page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 # Create your views here.
@@ -16,6 +17,8 @@ def product_detail(request, slug):
 
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(is_available=True)
@@ -24,5 +27,7 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
-    return render(request, 'main/product/list.html', {'category' : category, 'categories' : categories, 'products' : products})
+    paginator = Paginator(products, 4)
+    current_page = paginator.page(int(page))
+    return render(request, 'main/product/list.html', {'category' : category, 'categories' : categories, 'products' : current_page, 'slug_url' : category_slug})
 
